71ed0ba434822d596bc3cdf94ca56e1b9867c2e5
cosyprang <cosyprang@gmail.com>
2024-03-13 00:21:03 +0700

    Update Lastest

--- public/javascripts/linktopages.js
+++ public/javascripts/linktopages.js
@@ -2,22 +2,6 @@ function linkto_order(){
     window.location.href = '/order';
 }
 
-function linkto_noodles(){
-    window.location.href = 'order/noodles';
-}
-
-function linkto_meat(){
-    window.location.href = 'order/meat';
-}
-
-function linkto_topping(){
-    window.location.href = 'order/topping';
-}
-
-function linkto_addon(){
-    window.location.href = 'order/add-on';
-}
-
 function Backward (){
     window.history.back();
 }
