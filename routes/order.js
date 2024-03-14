ac18abd822bbe2085bad83bbb4aefccfb1bf651a
cosyprang <cosyprang@gmail.com>
2024-03-14 11:02:05 +0700

    Create Router for showData(Table) Page

--- routes/order.js
+++ routes/order.js
@@ -6,20 +6,27 @@ router.get('/', function (req, res, next) {
     res.render('order/order',{ title: 'Order'});
 });
 
-router.get('/noodles', function (req, res, next) {
-    res.render('order/noodles',{ title: 'Noodles'});
-});
+// router.get('/noodles', function (req, res, next) {
+//     res.render('order/noodles',{ title: 'Noodles'});
+// });
 
-router.get('/meat', function (req, res, next) {
-    res.render('order/meat',{ title: 'Meat'});
-});
+// router.get('/meat', function (req, res, next) {
+//     res.render('order/meat',{ title: 'Meat'});
+// });
 
-router.get('/topping', function (req, res, next) {
-    res.render('order/topping',{ title: 'Topping'});
-});
+// router.get('/topping', function (req, res, next) {
+//     res.render('order/topping',{ title: 'Topping'});
+// });
 
-router.get('/add-on', function (req, res, next) {
-    res.render('order/add-on',{ title: 'Add-on'});
+// router.get('/add-on', function (req, res, next) {
+//     res.render('order/add-on',{ title: 'Add-on'});
+// });
+
+
+///////////// Link to Show Data Table  /////////////
+
+router.get('/showData', function (req, res, next) {
+    res.render('order/showData',{ title: 'showData'});
 });
 
 module.exports = router;
