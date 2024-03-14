7cf51c4c98e3e2d993b6d86fad587d0a2d3c504c
cosyprang <cosyprang@gmail.com>
2024-03-14 11:06:23 +0700

    Del index path

--- routes/home.js
+++ routes/home.js
@@ -6,8 +6,4 @@ router.get('/', function (req, res, next) {
     res.render('home',{ title: 'Homepage'});
 });
 
-router.get('/index', function (req, res, next) {
-    res.render('index',{ title: 'index'});
-});
-
 module.exports = router;
